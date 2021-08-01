7dc98fb963c8fc94d2a2a77730458e4227a2e4d8
vnylbscr <mertgenc8@gmail.com>
2021-08-01 18:41:13 +0300

    fixed generate types

---
+++ src/components/BaseComponents/Button/index.tsx
@@ -0,0 +1,43 @@
+import { makeStyles } from '@material-ui/styles';
+import React, { FC } from 'react';
+import { MyTheme } from '../../../styles/config';
+import { Button } from '@material-ui/core';
+interface IProps {
+   fullWidth?: boolean;
+   style?: object;
+   onClick: () => void;
+   label?: string;
+   width?: number;
+   startIcon?: React.ReactNode;
+   endIcon?: React.ReactNode;
+   [x: string]: any;
+}
+
+const useStyles = makeStyles<MyTheme, IProps>((theme: MyTheme) => ({
+   root: {
+      color: theme.colorPalette.secondary,
+      backgroundColor: theme.colorPalette.primary.main,
+      borderRadius: 10,
+      textTransform: 'none',
+      width: (props) => (props.width ? props.width : ''),
+   },
+}));
+
+const MyButton = (props: IProps) => {
+   const { label, onClick, style, fullWidth, ...rest } = props;
+   const classes = useStyles(props);
+   return (
+      <Button
+         className={classes.root}
+         variant='outlined'
+         style={style}
+         onClick={onClick}
+         fullWidth={fullWidth}
+         {...rest}
+      >
+         {label}
+      </Button>
+   );
+};
+
+export default MyButton;
