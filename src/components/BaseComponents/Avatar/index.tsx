aa866c827b5da2bcc0c8db91b2b60e74d865bec7
vnylbscr <mertgenc8@gmail.com>
2021-09-09 12:22:21 +0300

    adding avatar components untracked.

---
+++ src/components/BaseComponents/Avatar/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Avatar } from 'material-ui';
+import { MyTheme } from '../../../styles/config';
+import { ColorsKey, VariantSize } from '../../../types/helperTypes';
+
+interface IProps {
+   size?: number | VariantSize | undefined;
+   color?: ColorsKey | undefined;
+}
+const useStyles = makeStyles<MyTheme, IProps>((theme: MyTheme) => ({
+   root: {
+      width: (props) => {
+         if (props.size === 'small') {
+            return 47;
+         } else if (props.size === 'medium') {
+            return 72;
+         }
+      },
+   },
+}));
+const CustomAvatar: FC<IProps> = (props) => {
+   const classes = useStyles(props);
+   const { size, color = 'pink' } = props;
+   return <Avatar className={classes.root}></Avatar>;
+};
+
+export default CustomAvatar;
